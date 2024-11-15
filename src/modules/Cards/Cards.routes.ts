836ad83d8ab26d21f7e584d0379dc6235f396b8e
BrunoWatanabe <bruno_watanabe@discente.ufg.br>
2024-11-15 13:19:56 -0300

    feat: endpoint crud de cards
    criação dos endpoints crud da tabela card

---
+++ src/modules/Cards/Cards.routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from 'fastify';
+import CardsController from './Cards.controller';
+
+async function cardsRoutes(fastify: FastifyInstance) {
+  fastify.get('/cards', CardsController.findAll);
+  fastify.get('/cards/:id', CardsController.findById);
+  fastify.post('/cards', CardsController.create);
+  fastify.put('/cards/:id', CardsController.update);
+  fastify.delete('/cards/:id', CardsController.delete);
+  fastify.get('/cards/assigned/:userId', CardsController.findByAssignedUser);
+  fastify.get('/cards/sprint/:sprintId', CardsController.findBySprint);
+}
+
+export default cardsRoutes;
---
+++ src/modules/Cards/Cards.service.ts
@@ -0,0 +1,82 @@
+import prisma from "../../clients/prisma.client";
+import { CardsDto } from "./schemas/create-Cards.schema";
+
+class CardsService {
+  async findAll() {
+    try {
+      return await prisma.cards.findMany();
+    } catch (error) {
+      throw new Error("Falha ao recuperar os cards");
+    }
+  }
+
+  async findById(id: number) {
+    try {
+      const card = await prisma.cards.findUnique({
+        where: { id },
+      });
+
+      if (!card) throw new Error("Card não encontrado");
+
+      return card;
+    } catch (error) {
+      throw new Error("Falha ao encontrar o card");
+    }
+  }
+
+  async create(createCardDto: CardsDto) {
+    try {
+      console.log("Dados enviados ao Prisma:", createCardDto);
+      return await prisma.cards.create({
+        data: createCardDto,
+      });
+    } catch (error) {
+      console.error("Erro no Prisma:", error);
+      throw new Error("Falha ao criar o card");
+    }
+  }
+  
+
+  async update(id: number, updateCardDto: Partial<CardsDto>) {
+    try {
+      return await prisma.cards.update({
+        where: { id },
+        data: updateCardDto,
+      });
+    } catch (error) {
+      throw new Error("Falha ao atualizar o card");
+    }
+  }
+
+  async delete(id: number) {
+    try {
+      return await prisma.cards.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw new Error("Falha ao deletar o card");
+    }
+  }
+
+  async findByAssignedUser(userId: number) {
+    try {
+      return await prisma.cards.findMany({
+        where: { assigned: userId },
+      });
+    } catch (error) {
+      throw new Error("Falha ao recuperar os cards atribuídos ao usuário");
+    }
+  }
+
+  async findBySprint(sprintId: number) {
+    try {
+      return await prisma.cards.findMany({
+        where: { sprint: sprintId },
+      });
+    } catch (error) {
+      throw new Error("Falha ao recuperar os cards para o sprint");
+    }
+  }
+}
+
+export default new CardsService();
