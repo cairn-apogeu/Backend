1dee27d1c831570a72004692e16133c64918afc7
JOTAPEnvz <jpmoraistomaz01@gmail.com>
2024-12-15 01:51:08 -0300

    feat: add campos na tabela projetos

--- src/modules/projetos/schemas/to-projetos.schema.ts
+++ src/modules/projetos/schemas/to-projetos.schema.ts
@@ -6,6 +6,9 @@ export const ToProjetosSchema = z.object({
   status: z.string(),
   id_cliente: z.string(),
   id_gestor: z.string(),
+  token: z.string(),
+  repositorio: z.string(),
+  owner: z.string(),
 });
 
 export type ToProjetosDto = z.infer<typeof ToProjetosSchema>;
