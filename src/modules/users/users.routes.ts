397d6bd8243db5cec3286b2b35466a7705909c5c
BrunoWatanabe <bruno_watanabe@discente.ufg.br>
2025-01-10 18:46:54 -0300

    feat: endpoints para projetos do usuário
    Adição dos endpoints findUserbyProjectId e findProjectByUserId

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -4,6 +4,7 @@ import userController from "./users.controller";
 export default async function userRoutes(app: FastifyInstance) {
   app.get("/users", userController.findAll);
   app.get("/users/:id", userController.findById);
+  app.get("/users/project/:id", userController.findUserbyProjectId); 
   app.post("/users", userController.newUser);
   app.put("/users/:id", userController.updateUser);
   app.delete("/users/:id", userController.deleteUser);
